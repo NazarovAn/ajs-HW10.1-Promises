81cf4f1ea349774641fa985e48779d3a6ba12632
NazarovAn <naatonchrome@gmail.com>
2020-05-25 17:45:14 +0400

    hw done

---
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -0,0 +1,8 @@
+import GameSavingLoader from '../GameSavingLoader';
+
+test('GameSavingLoader', (done) => {
+  GameSavingLoader.load().then((saving) => {
+    expect(saving).toBe('{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}');
+    done();
+  });
+});
